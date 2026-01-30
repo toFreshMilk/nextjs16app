62d677608fbe7f4bed9f371c880d68aaad0809c0
Seongmin <lsm@buptle.com>
2026-01-30 17:40:53 +0900

    .

--- src/core/hooks/useTenant.ts
+++ src/core/hooks/useTenant.ts
@@ -1,6 +1,7 @@
 // src/core/hooks/useTenant.ts
 import { useAppConfig } from '@/core/contexts/AppConfigContext';
 
+//이건 삭제해도 되겠는데
 export function useTenant() {
   const { config } = useAppConfig();
   return {
