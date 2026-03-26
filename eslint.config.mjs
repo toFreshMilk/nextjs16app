99ea9c172611818867be6a624eeafdef9af5086b
Seongmin <lsm@buptle.com>
2026-03-26 16:03:34 +0900

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ const eslintConfig = defineConfig([
       '@typescript-eslint/no-unused-vars': 'warn',
     },
   },
-  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts', 'playwright-report/**', 'test-results/**']),
 ]);
 
 export default eslintConfig;
